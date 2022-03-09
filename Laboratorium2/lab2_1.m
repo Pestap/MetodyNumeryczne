47ff17d9b5401c90b93162e684f54d0d84d0d6fe
Pestap <pestap4k@gmail.com>
2022-03-09 21:39:58 +0100

    Laborka 2 - zmiany na zajęciach
    - naprawienie średniej
    - wykresy się zapisują
    - dodanie opisów osi na wykresach
    - tytuły wykresów

--- Laboratorium2/lab2_1.m
+++ Laboratorium2/lab2_1.m
@@ -3,13 +3,12 @@ N=7;
 
 I=speye(N)
 
-w1=sparse([4 6 3 4 5 5 6 7 5 6 4 6 4 7 6])
 w2=sparse([1 1 2 2 2 3 3 3 4 4 5 5 6 6 7])
-w3=sparse([1 1 1 1 1 1 1 1 1 1 1 1 1 1 1])
+w1=sparse([4 6 3 4 5 5 6 7 5 6 4 6 4 7 6])
 
-B=sparse(w1,w2,w3,N,N)
+B=sparse(w1,w2,1,N,N)
 
-L=sum(B).' %L to wektor sum wierszy
+L=sum(B).' %L to wektor sum wierszy macierzy B
 
 b=sparse(N,1)
 b(:,:) = (1-d)/N
@@ -17,6 +16,6 @@ b(:,:) = (1-d)/N
 A=spdiags(1./L,0,N,N)
 
 M = I - d*B*A
-r = sparse(M\b)
+r = M\b
 
 bar(r)
--- Laboratorium2/lab2_2.m
+++ Laboratorium2/lab2_2.m
@@ -1,35 +1,33 @@
-
-
-
 a = 1;
-r_max = a/4;
+r_max = a/3;
 n_max = 1000;
 
 X = 0;
 Y = 0;
 R = 0;
-x = [];
-y = [];
-r = [];
-p = [];
-pSum = []
-tries = []
+x = zeros(1,n_max);
+y = zeros(1,n_max);
+r = zeros(1,n_max);
+p = zeros(1,n_max);
+pSum = zeros(1,n_max);
+tries = zeros(1, n_max);
 n = 0;
-
+numberOfTries = 0;
 
 while n < n_max
 
     fitsInA = false;
-    numberOfTries = 0;
+    
     while not(fitsInA)
        
-        R = rand()*r_max;
-        X = rand()*a;
-        Y = rand()*a;
+        R = rand*r_max;
+        X = rand*a;
+        Y = rand*a;
         
         if X + R < a && X - R > 0 && Y + R < a && Y-R > 0
-            fitsInA = true
+            fitsInA = true;
         end
+
          numberOfTries = numberOfTries + 1;
     end
     
@@ -37,15 +35,14 @@ while n < n_max
     for i = 1:n
         distanceBetweenCenters = sqrt((x(i)-X)^2 + (y(i) - Y)^2);
         radiusSum = r(i) + R;
-        if distanceBetweenCenters <radiusSum
-            intersects = true
+        if distanceBetweenCenters < radiusSum
+            intersects = true;
             break;  
         end
-        
-
     end
+
     if not(intersects)
-        n = n+1;
+        n = n + 1;
         area = pi*R*R;
         plot_circ(X,Y,R);
         x(n) = X;
@@ -53,6 +50,7 @@ while n < n_max
         r(n) = R;
         p(n) = area;
         tries(n) = numberOfTries;
+        numberOfTries = 0;
         if n == 1
             pSum(n) = area;
         else
@@ -61,15 +59,28 @@ while n < n_max
 
         axis equal
         hold on
-        fprintf(1, ' %s%5d%s%.3g\r ', 'n =',  n, ' S = ', area);
+        %fprintf(1, ' %s%5d%s%.3g\r ', 'n =',  n, ' S = ', area);
         pause(0.01);
     end
 
 end
 hold off
 
-figure('Name', 'Powierzchnia łączna');
-semilogx(pSum);
 
+title('Okręgi w kwadracie');
+saveas(gcf, 'okregi.png');
+
+figure('Name', 'Powierzchnia łączna');
+
+
+semilogx(pSum);
+title('Powierzchnia łączna');
+xlabel('n-ty okrąg');
+ylabel('Powierzchnia');
+saveas(gcf, 'powierzchnia.png');
 figure('Name', 'Liczba prób');
-loglog(cumsum(tries))
+loglog(cumsum(tries) ./ ((1:n_max)))
+title('Liczba prób');
+xlabel('n-ty okrąg');
+ylabel('liczba prób');
+saveas(gcf, 'liczbaprob.png');
