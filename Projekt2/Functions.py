aade54a13b2088236a02f8fd5305bacecb78c9e1
Pestap <90579181+Pestap@users.noreply.github.com>
2022-04-14 19:43:34 +0200

    Zad2 - funkcje pomocnicze: - odrwacanie macierzy D

--- Projekt2/Functions.py
+++ Projekt2/Functions.py
@@ -25,20 +25,103 @@ def generateMatrixExA():
 def generateVectorBexA():
     N = 27
     f = 4
-    vector = [0]*N
+    vector = []
     for i in range(N):
-        vector[i] = math.sin(i*(f+1))
+        vector.append([math.sin(i*(f+1))])
     return vector
+
+
+def getLUD(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    L = []
+    U = []
+    D = []
+
+    #macierz D
+    for i in range(rows):
+        row = [0]*cols
+        for j in range(cols):
+            if i == j:
+                row[j] = matrix[i][j]
+                break #interesuje nas tylko przekatna
+        D.append(row)
+
+    #macierz L
+    for i in range(rows):
+        row = [0]*cols
+        for j in range(cols):
+            if j < i:
+                row[j] = matrix[i][j]
+        L.append(row)
+
+    #macierz U
+    for i in range(rows):
+        row = [0]*cols
+        for j in range(cols):
+            if j > i:
+                row[j] = matrix[i][j]
+        U.append(row)
+
+    return L, U, D
+
+
+def sumMatrices(A, B):
+    if not (len(A) == len(B) and len(A[0]) == len(B[0])):
+        raise Exception("Błędne rozmiary macierzy prz dodawaniu")
+
+    result = []
+
+    for i in range(len(A)):
+        row = [0]* len(A)
+        for j in range(len(A[0])):
+            row[j] = A[i][j] + B[i][j]
+        result.append(row)
+
+    return result
+
+
+def multiplyMatrices(A,B):
+    #sprawdzam rozmiar macierzy
+    rowsA = len(A)
+    colsA = len(A[0])
+
+    rowsB = len(B)
+    colsB = len(B[0])
+
+    if not(colsA == rowsB):
+        raise Exception("Błędny rozmiar macierzy - mnożenie")
+
+    #TODO: faktycznie mnożenie macierzy
+#TODO: i tak
+
+
+def inverseDiagonalMatrix(matrix):
+    result = []
+    for i in range(len(matrix)):
+        row = [0] * len(matrix[0])
+        for j in range(len(matrix[0])):
+            if i == j:
+                row[j] = 1 / matrix[i][j]
+                break # intersuje nas tylko przekątna
+        result.append(row)
+
+    return result
 
 
 def printMatrix(matrix):
     rows = len(matrix)
+    print()
     for i in range(rows):
         for j in matrix[i]:
             print(j, end=" ")
         print()
+    print()
+
 
 def printVector(vector):
+    print()
     for i in vector:
-        print(i, end=" ")
+        print(i)
     print()
--- Projekt2/main.py
+++ Projekt2/main.py
@@ -5,5 +5,17 @@ import Functions
 if __name__ == '__main__':
     matrix = Functions.generateMatrixExA()
     vectorB = Functions.generateVectorBexA()
-    Functions.printMatrix(matrix)
-    Functions.printVector(vectorB)
+
+    L, U, D = Functions.getLUD(matrix)
+
+
+    Functions.printMatrix(vectorB)
+    Functions.printMatrix(D)
+    D_1 = Functions.inverseDiagonalMatrix(D)
+    Functions.printMatrix(D_1)
+    Functions.printMatrix(U)
+    Functions.printMatrix(L)
+
+    R = Functions.sumMatrices(U, L)
+
+
