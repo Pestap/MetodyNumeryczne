ad6b786acf4ce7dc8325cfc03fde8d3b086e29e4
Pestap <90579181+Pestap@users.noreply.github.com>
2022-04-23 23:37:51 +0200

    Działające Forward i Backward substitution v2 - korekta w mainie

--- Projekt2/main.py
+++ Projekt2/main.py
@@ -4,7 +4,7 @@ from GaussSeidel import gaussSeidel
 
 
 if __name__ == '__main__':
-    N = 131
+    N = 931
     matrix = Functions.generateBandMatrix(10, -1, -1, N)
     vectorB = Functions.generateBVector(N)
 
@@ -14,11 +14,10 @@ if __name__ == '__main__':
     Functions.printMatrix(jacobiResult)
     print("Liczba iteracji: " + str(jacobiIterations))
 
-    gaussSeidelResult, gaussSeidelIterations, time = gaussSeidel(matrix, vectorB, 10**(-6))
+    gaussSeidelResult, gaussSeidelIterations = gaussSeidel(matrix, vectorB, 10**(-6))
 
     Functions.printMatrix(gaussSeidelResult)
     print("Liczba iteracji: " + str(gaussSeidelIterations))
-    print("TIME: " + str(time))
 
     print("Porównanie wyników:")
     for i in range(len(jacobiResult)):
