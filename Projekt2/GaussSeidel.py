599c9d4be26b4f8567ea13576595849c7ab6e907
Pestap <90579181+Pestap@users.noreply.github.com>
2022-05-04 19:39:35 +0200

    Sprawozdanie + drobne zmiany
    Prawie skończone sprawozdanie + drobna zmiana w GS - drugi czynnik obliczany przed pętlą

--- Projekt2/GaussSeidel.py
+++ Projekt2/GaussSeidel.py
@@ -20,12 +20,13 @@ def gaussSeidel(matrix, vector, epsilon):
     start = time.time()
 
     DL = matrixAddition(D, L)
+    second = forwardSubstitution(DL, vector)
 
     initialRes = calculateResiduumNorm(matrix,xk, vector)
     while calculateResiduumNorm(matrix, xk, vector) > epsilon:
         # Jako, że macierz DL jest macierzą dolną trójkątną, to do rozwiązania układów równań możemy skorzystać
         # z metody podstawiania w przód, co daje dużo lepsze wyniki niż użycie do tego celu metody Jacobiego
-        second = forwardSubstitution(DL, vector)
+
         UR = matrixMultiplication(U, xk)
         first = scalarMatrixMultiplication(forwardSubstitution(DL, UR), -1)
         iterations += 1
--- Projekt2/main.py
+++ Projekt2/main.py
@@ -1,12 +1,12 @@
 import Functions
-import matplotlib.plot as plt
+import matplotlib.pyplot as plt
 from Jacobi import jacobi
 from GaussSeidel import gaussSeidel
 from LU import LU
 
 
 if __name__ == '__main__':
-    N = 131
+    N = 931
     epsilon = 10 ** (-9)
     matrixA = Functions.generateBandMatrix(10, -1, -1, N)
     matrixC = Functions.generateBandMatrix(3,-1,-1, N)
@@ -25,9 +25,9 @@ if __name__ == '__main__':
         print(str(jacobiResult[i]) + " --- " + str(gaussSeidelResult[i]) + " --- " + str(LUresult[i]))
 
     print("\nPorównanie wyników:")
-    print("Jacobi: czas - " + str(jacobiTime) + "s , iteracje - " + str(jacobiIterations) + "norma z wektora residuum: " + str(Functions.calculateResiduumNorm(matrixA, jacobiResult, vectorB)))
-    print("Gauss-Seidl: czas - " + str(gaussSeidelTime)+ "s , iteracje - " + str(gaussSeidelIterations) + "norma z wektora residuum: " + str(Functions.calculateResiduumNorm(matrixA, gaussSeidelResult, vectorB)))
-    print("Faktoryzacja LU: czas -  " + str(LUtime) + "s, " + "norma z wektora residuum: " + str(Functions.calculateResiduumNorm(matrixA, LUresult, vectorB)))
+    print("Jacobi: czas - " + str(jacobiTime) + "s , iteracje - " + str(jacobiIterations) + ", norma z wektora residuum: " + str(Functions.calculateResiduumNorm(matrixA, jacobiResult, vectorB)))
+    print("Gauss-Seidl: czas - " + str(gaussSeidelTime)+ "s , iteracje - " + str(gaussSeidelIterations) + ", norma z wektora residuum: " + str(Functions.calculateResiduumNorm(matrixA, gaussSeidelResult, vectorB)))
+    print("Faktoryzacja LU: czas -  " + str(LUtime) + " s, " + "norma z wektora residuum: " + str(Functions.calculateResiduumNorm(matrixA, LUresult, vectorB)))
     print()
 
     #Zad C i D
@@ -43,21 +43,33 @@ if __name__ == '__main__':
     #TODO: Wykres czasu trwania od liczby niewiadomych
 
     nArray = [100, 500, 1000, 2000, 3000]
-    timesJacobi = [0]*len(nArray)
-    timesGS = [0]*len(nArray)
-    timesLU = [0]*len(nArray)
+    #nArray = [10, 20, 30, 50, 100, 200, 500]
+    timesJacobi = []
+    timesGS = []
+    timesLU = []
 
     for i in nArray:
-        jacobiItime = jacobi(matrixA, vectorB, epsilon)[2]
-        gsItime = gaussSeidel(matrixA, vectorB, epsilon)[2]
-        luTime = LU(matrixA, vectorB)[1]
-        timesJacobi[i] = jacobiItime
-        timesGS[i] = gsItime
-        timesLU[i] = luTime
+        matrixI = Functions.generateBandMatrix(10, -1, -1, i)
+        vectorBI = Functions.generateBVector(i)
+
+        jacobiItime = jacobi(matrixI, vectorBI, epsilon)[2]
+        gsItime = gaussSeidel(matrixI, vectorBI, epsilon)[2]
+        luTime = LU(matrixI, vectorBI)[1]
+        timesJacobi.append(jacobiItime)
+        timesGS.append(gsItime)
+        timesLU.append(luTime)
 
 
-
-
+    plt.plot(nArray, timesJacobi)
+    plt.plot(nArray, timesGS)
+    plt.plot(nArray, timesLU)
+    plt.title("Porównanie czasu działania metod w zależności od liczby zmiennych")
+    plt.xlabel("Liczba zmiennych N")
+    plt.ylabel("Czas [s]")
+    plt.yscale('log')
+    plt.legend(["Jacobi", "Gauss-Seidel", "Faktoryzacja LU"])
+    plt.savefig("Images/wykresZadE.png")
+    plt.show()
 
 
     #TODO: Porównanie z gotowcami z numpy, ale to zrobić w oddzielnym pliku
