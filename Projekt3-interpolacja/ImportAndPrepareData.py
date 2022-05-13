0c9df635467161a8f22851de5882f020b626d671
Pestap <90579181+Pestap@users.noreply.github.com>
2022-05-13 10:23:55 +0200

    Zmiana w prepareData - zamiast ostatniego krótkiego przedziału - ostatni trochę dłuższy

--- Projekt3-interpolacja/ImportAndPrepareData.py
+++ Projekt3-interpolacja/ImportAndPrepareData.py
@@ -30,18 +30,28 @@ def prepareData(filename, n):
     # musimy wziąć n punktów w równych odstępach
 
     interval = len(points) // (n-1)
+
+    #w liscie interpolation points mamy: wartośc funkcji w punkcie x,y oraz indeks
     interpolation_points = []
     for idx, point in enumerate(points):
         if idx % interval == 0:
             interpolation_points.append((point[2], idx))
+
+    #dodajemy ostatni punkt pomiarowy
     interpolation_points.append((points[-1][2], len(points)-1))
+    #usuwamy przedostatni - ostatni przedział będzie dłuższy niż pozostałe(lepiej niż bardzo mały)
+    interpolation_points.pop(-2)
 
 
-
-
+    #rozpakowujemy punkty
+    #pobieramy wartosci funkcji dla wszystkich punktów
     elevations_all_values = [elevation[2] for elevation in points]
+    #pobieramy wszystkie wartosci xow - kolejne numery pomiarów
     elevations_all_x = [i for i in range(len(points))]
+
+    #pobieramy wartości funkcji w węzłach interpolacji
     elevations_interpolation_values = [point[0] for point in interpolation_points]
+    #pobieramy wartości x-ow w węzłach interpolacji
     elevations_interpolation_x = [point[1] for point in interpolation_points]
 
-    return elevations_all_values,elevations_all_x, elevations_interpolation_values, elevations_interpolation_x
+    return elevations_all_values, elevations_all_x, elevations_interpolation_values, elevations_interpolation_x
--- Projekt3-interpolacja/main.py
+++ Projekt3-interpolacja/main.py
@@ -16,11 +16,11 @@ x_val = np.linspace(1,5,40)
 #y_res = lagrange(xarr, yarr, x_val)
 #plt.plot(x_val, y_res)
 
-elevations_all_values,elevations_all_x, elevations_interpolation_values, elevations_interpolation_x = prepareData("track_points.csv", 12)
+elevations_all_values,elevations_all_x, elevations_interpolation_values, elevations_interpolation_x = prepareData("track_points.csv", 5)
 
-#interpolation_result = lagrange(elevations_interpolation_x, elevations_interpolation_values, elevations_all_x)
+interpolation_result = lagrange(elevations_interpolation_x, elevations_interpolation_values, elevations_all_x)
 
-interpolation_result = splines(elevations_interpolation_x, elevations_interpolation_values, elevations_all_x)
+#interpolation_result = splines(elevations_interpolation_x, elevations_interpolation_values, elevations_all_x)
 
 #interpolation_result = splines(xarr,yarr, x_val)
 
